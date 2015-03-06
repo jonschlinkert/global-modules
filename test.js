9cf00ecb75c1da4f0f89bb79e51f9601ca87ef89
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-06 08:15:23 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * global-modules <https://github.com/jonschlinkert/global-modules>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var assert = require('assert');
+var mods = require('./');
+
+describe('mods', function () {
+  it('should resolve the path to the global npm modules directory:', function () {
+    assert(fs.existsSync(mods) === true);
+  });
+});
