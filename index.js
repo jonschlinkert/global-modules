650012e4b420b4d470e1e80c73d0a1e2fb4fb34f
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-15 01:21:50 -0400

    use is-windows

--- index.js
+++ index.js
@@ -9,8 +9,9 @@
 
 var path = require('path');
 var prefix = require('global-prefix');
+var isWindows = require('is-windows');
 
-if (process.platform === 'win32') {
+if (isWindows()) {
   module.exports = path.resolve(prefix, 'node_modules');
 } else {
   module.exports = path.resolve(prefix, 'lib/node_modules');
