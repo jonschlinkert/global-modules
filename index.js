e5e85ca2221f84e7acf443950caa159e81416ba3
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-05 05:35:24 -0400

    update deps

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var path = require('path');
 var prefix = require('global-prefix');
 var isWindows = require('is-windows');
 
-if (isWindows === true || isWindows()) {
+if (isWindows()) {
   module.exports = path.resolve(prefix, 'node_modules');
 } else {
   module.exports = path.resolve(prefix, 'lib/node_modules');
