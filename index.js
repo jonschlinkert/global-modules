efae3113ee92619c926454d03d5ce264ae6fc358
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-28 02:13:42 -0400

    run update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * global-modules <https://github.com/jonschlinkert/global-modules>
  *
- * Copyright (c) 2015 Jon Schlinkert.
+ * Copyright (c) 2015-2017 Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
